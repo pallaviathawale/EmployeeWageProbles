cb0ea72b11bcdc5fa387e8858480fbd04e88245b
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-02-17 20:17:40 +0530

    Add: file

---
+++ EmployeeWage260/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace EmployeeWage260
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+    }

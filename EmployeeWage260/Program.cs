de9ba5d464ea956fc0f405ec162b2602d6c08af9
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-02-17 22:25:53 +0530

    Add: UC7-EmpWageUsingClassMethod

--- EmployeeWage260/Program.cs
+++ EmployeeWage260/Program.cs
@@ -2,45 +2,54 @@
 {
     internal class Program
     {
+
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        const int NUM_OF_WORKING_DAYS = 2;
+        const int MAX_HRS_IN_MONTH = 10;
+        public static int computeEmpWage()
+        {
+
+            int empHrs = 0, totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+            }
+
+            totalEmpHrs += empHrs;
+            Console.WriteLine("Day:" + totalWorkingDays + "Emp Hrs:" + empHrs);
+
+
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Emp Wage :" + totalEmpWage);
+            return totalEmpWage;
+        }
+
         static void Main(string[] args)
         {
-            {
-                const int IS_PART_TIME = 1;
-                const int IS_FULL_TIME = 2;
-                const int EMP_RATE_PER_HOUR = 20;
-                const int NUM_OF_WORKING_DAYS = 2;
-                const int MAX_HRS_IN_MONTH = 10;
-                int empHrs = 0, totalEmpHrs = 0;
-                int totalWorkingDays = 0;
-                while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
-                {
-                    totalWorkingDays++;
-                    Random random = new Random();
-                    int empCheck = random.Next(0, 3);
-                    switch (empCheck)
-
-                    {
-                        case IS_FULL_TIME:
-                            empHrs = 8;
-                            break;
-                        case IS_PART_TIME:
-                            empHrs = 4;
-                            break;
-                        default:
-                            empHrs = 0;
-                            break;
-                    }
-                }
-                
-                totalEmpHrs += empHrs;
-                Console.WriteLine("Day:" +totalWorkingDays + "Emp Hrs:" +empHrs);
-                int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-                Console.WriteLine("Total Emp Wage :" +totalEmpWage);
-            }
-            
-            
+            computeEmpWage();
         }
     }
-}
+}      
+            
+        
+   
     
     
